15a401bf90f070b3e7b7500a97813d8b2fe5f17c
Yasmeena Hilal <yasmeenahilal1@gmail.com>
2025-03-15 18:00:02 +0530

    Completed Jwt Refresh and Access Token

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,6 +12,8 @@ class Settings(BaseSettings):
     JWT_ALGORITHM: str
     JWT_TOKEN_VALIDITY: int
     REFRESH_TOKEN_VALIDITY: int
+    REDIS_HOST: str
+    REDIS_PORT: int
 
     model_config = SettingsConfigDict(
         env_file=".env",
